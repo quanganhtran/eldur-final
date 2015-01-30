24cb895690d88afbf1d6432afb1f1690ac9c825c
Anh <Anh@192.168.197.1>
2015-01-30 18:55:44 +0200

    Main class

---
+++ src/eldur/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eldur;
+
+/**
+ *
+ * @author Anh
+ */
+public class Main {
+    public static void main(String[] args) {
+        Eldur eldur = new Eldur();
+        eldur.game();
+    }
+}
