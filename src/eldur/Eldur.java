2d0e48e0e5e0d5a43228844dd49b22bcf8c92c4e
Anh Tran <quanganh.tran94@gmail.com>
2015-01-30 11:16:45 +0200

    Beta Crafting System

--- src/eldur/Eldur.java
+++ src/eldur/Eldur.java
@@ -6,6 +6,7 @@
 package eldur;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -17,19 +18,49 @@ public class Eldur {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         // Statistics
-
-
+        iron = 10;
+        copper = 10;
+        
+        // Initialization
+        Recipe ironSwordRecipe = new Recipe("Iron Sword", "Iron Sword", 10, 1, 20, 5);
+        Recipe copperSwordRecipe = new Recipe("Copper Sword", "Copper Sword", 1, 10, 15, 10);
+        
+        // Game
+        System.out.println("Type a command");
+        command = reader.nextLine();
+        while (!command.equals("quit")) {
+            String[] input = command.split("\\s");
+            if (input[0].equals("stat")) {
+                System.out.println("Iron "+iron+" Copper "+copper);
+                System.out.println(inventoryViewer);
+            } else if (input[0].equals("craft") && input.length == 2) {
+                if (input[1].equals("iron")) {
+                    craft(ironSwordRecipe);
+                } else if (input[1].equals("copper")) {
+                    craft(copperSwordRecipe);
+                }
+            }
+            System.out.println("Type a command");
+            command = reader.nextLine();
+        }
         
     }
     
     //public int cash;
-    public int iron;
-    public int copper;
-    public ArrayList<Sword> inventory = new ArrayList<Sword>();
-    public void craft(Recipe someRp) {
+    public static int iron;
+    public static int copper;
+    public static String command;
+    
+    public static ArrayList<Sword> inventory = new ArrayList<Sword>();
+    public static ArrayList<String> inventoryViewer = new ArrayList<String>();
+    public static void craft(Recipe someRp) {
         iron -= someRp.getCostIron();
         copper -= someRp.getCostCopper();
-        inventory.add(new Sword(someRp));
+        Sword newSword = new Sword(someRp);
+        inventory.add(newSword);
+        inventoryViewer.add(newSword.getName() + " " + newSword.getAtk() + " " + newSword.getSpd());
+        System.out.println("Sword crafted: "+newSword.getName() + " " + newSword.getAtk() + " " + newSword.getSpd());
     }
 }
