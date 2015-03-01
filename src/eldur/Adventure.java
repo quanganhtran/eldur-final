909c079fc77ccfaffa86403f396952b1afb7b10a
Anh <Anh@Aquamarine>
2015-03-01 06:25:10 +0200

    Home, 1 Mar, Commit 1 (major) - Areas and Commands

--- src/eldur/Adventure.java
+++ src/eldur/Adventure.java
@@ -18,13 +18,11 @@ public class Adventure extends Screen {
     private Random rng = new Random();
     private GameData gameData;
     private Character player;
-    private int areaId;
     private ArrayList<Enemy> enemyTypes;
 
     public Adventure(String sN, GameData gD) {
         super(sN);
         this.gameData = gD;
-        this.areaId = 1;
         this.enemyTypes = new ArrayList<>();
         //System.out.println(enemyTypes.size());
     }
@@ -40,32 +38,31 @@ public class Adventure extends Screen {
         //Enemy enemy = new Enemy("Slime", 100, 100);
         while (true) {
             Encounter encounter = new Encounter(new Enemy(enemyTypes.get(rng.nextInt(enemyTypes.size()))));
-            System.out.println("Do you want to engage? ('y' for yes)");
-            String oocInput = reader.nextLine();
-//            while (encounter.outcome.equals("outOfCombat")) {
-//                encounter.outOfCombat();
-//            }
-            if (this.connections.get(oocInput) != null) {
-                return this.connections.get(oocInput);
-            } else if (!oocInput.equals("y")) {
-                encounter.outcome = "pAvoid";
+            while (encounter.outcome.equals("outOfCombat")) {
+                encounter.outOfCombat();
             }
+//            if (this.connections.get(oocInput) != null) {
+//                return this.connections.get(oocInput);
+//            } else if (!oocInput.equals("y")) {
+//                encounter.outcome = "pAvoid";
+//            }
 
-            while (encounter.outcome.equals("")) {
+            while (encounter.outcome.equals("")) { // Turns happen until the encounter's outcome is decided.
 
-                String connection = encounter.turn();
-                if (connections.get(connection) != null) {// TODO Needs proper feedback
-                    return connections.get(connection);
-                }
+                //String getInput = encounter.turn();
+                encounter.turn();
+//                if (connections.get(connection) != null) {// TODO Needs proper feedback
+//                    return connections.get(connection);
+//                }
             }
             if (encounter.outcome.equals("pWin")) {
                 System.out.println("Another enemy incoming...");
-            } else {
-                if (encounter.outcome.equals("pLose")) {
-                    //System.out.println("debug");
-                    this.player = new Character(gameData);
-                    //encounter.outcome = "";
-                }
+            } else if (encounter.outcome.equals("pLose")) {
+                //System.out.println("debug");
+                this.player = new Character(gameData);
+                //encounter.outcome = "";
+            } else if (encounter.outcome.equals("switchScreen")) {
+                return connections.get(encounter.encInput);
             }
         }
     }
@@ -75,20 +72,6 @@ public class Adventure extends Screen {
         return input;
     }
 
-    /**
-     * @return the area
-     */
-    public int getAreaId() {
-        return areaId;
-    }
-
-    /**
-     * @param areaId the area to set
-     */
-    public void setAreaId(int areaId) {
-        this.areaId = areaId;
-    }
-    
     public void addEnemy(Enemy en) {
         this.enemyTypes.add(en);
     }
@@ -98,87 +81,77 @@ public class Adventure extends Screen {
         //private Character player;
         private Enemy enemy;
         private String outcome; // int here String below
+        private String encInput;
 
         public Encounter(Enemy en) {
             //this.player = c;
             this.enemy = en;
-            this.outcome = "";
+            this.outcome = "outOfCombat";
             System.out.println("A " + this.enemy.getName() + " has appeared!");
+            this.encInput = "";
 //            outcome = 0;
 //            while (outcome == 0) {
 //                outcome = turn();
 //            }
         }
 
-        private void outOfCombat() {
-            
+        public void outOfCombat() {
+            //OUTER:
+            //while (true) {
+            System.out.println("Do you want to engage? ('y' to engage, 'n' to skip)");
+            this.encInput = reader.nextLine();
+            switch (this.encInput) {
+                case "":
+                case "y":
+                    this.encInput = "";
+                    this.outcome = "";
+                    break;
+                case "n":
+                    // Skip the encounter.
+                    this.outcome = "pAvoid";
+                    break;
+                default:
+                    //this.outcome = "pAvoid";
+                    defaultHandler();
+            }
+            //}
         }
 
         public String turn() {
-            String input = "";
-            this.outcome = "noAction";
+            //String this.encInput = "";
+            //this.outcome = "noAction";
             preturn();
-            //while (turnOutcome.equals("noAction")) {
-            String[] afterAction = playerAction();
-            input = afterAction[0];
-            this.outcome = afterAction[1];
+            playerAction();
             if (this.outcome.equals("")) {
-                afterAction = enemyAction();
-                this.outcome = afterAction[1];
-//                if (!this.outcome.equals("pLose")) {
-                this.outcome = postturn(input, this.outcome);
-//                }
+                enemyAction();
+                if (!this.outcome.equals("pLose")) {
+                    postturn();
+                }
             }
-            //return new String[]{input, this.outcome};
-            return input;
+
+            return this.encInput;
         }
 
         public void preturn() {
 
         }
 
-        public String[] playerAction() {
-            String input = "";
-            String pOutcome = "noAction"; // Outcome of the actions of the player
-            while (pOutcome.equals("noAction")) {
+        private void playerAction() {
+            //String this.encInput = "";
+            this.outcome = "noAction"; // Outcome of the actions of the player
+            while (this.outcome.equals("noAction")) {
                 System.out.println("Your action: ");
-                input = reader.nextLine();
-                String[] inputParts = input.split("\\s");
+                this.encInput = reader.nextLine();
+                String[] inputParts = this.encInput.split("\\s");
                 // Resolve actions
                 switch (inputParts[0]) {
-                    case "where":
-                        reportScreen();
-                        System.out.println("The current area level is " + areaId + ".");
-                        System.out.println("a - Attack");
-                        System.out.println("equip <slot number>");
-                        System.out.println("back - Back to Town");
-                        input = "";
-                        //pOutcome = "noAction";
-                        break;
-                    case "quit":
-                        System.out.println("Thanks for playing!");
-                        System.exit(0);
-                    case "area":
-                        if (inputParts.length >= 2) {
-                            try {
-                                Integer.parseInt(inputParts[1]);
-                                pOutcome = "";
-                            } catch (NumberFormatException numberFormatException) {
-                                System.out.println("Area index must be a number.");
-                                input = "";
-                            }
-                        } else {
-                            System.out.println("Current area: " + areaId);
-                            input = "";
-                        }
-                        break;
                     case "a":
                     case "attack":
-                        input = "";
-                        pOutcome = "";
+                        this.encInput = "";
+                        this.outcome = "";
                         // begin Player will attack
-                        pOutcome = player.attack(enemy);
-                        if (!pOutcome.equals("pWin")) {
+                        this.outcome = player.attack(enemy);
+                        if (!this.outcome.equals("pWin")) {
                             System.out.println(enemy.getName() + "'s HP is " + enemy.getHp());
                         } else {
                             System.out.println("You have successfully defeated " + enemy.getName());
@@ -202,68 +175,67 @@ public class Adventure extends Screen {
                                 //gameData.setEquippedSword(sw);
                                 player.unsheathe(sw);
                                 System.out.println("You have equipped " + sw.getName() + ".");
-                                input = "";
-                                pOutcome = "";
+                                this.encInput = "";
+                                this.outcome = ""; // Equip successfully
                             } else {
                                 System.out.println("Sword not found.");
-                                input = "";
+                                this.encInput = "";
                             }
                         }
                         break;
                     default:
-                        if (connections.get(input) != null) {
-                            System.out.println("Do you really want to retreat from this encounter? ('y' to confirm)");
-                            if (reader.nextLine().equals("y")) {
-                                // input is not changed
-                                pOutcome = "switchScreen";
-                            } else {
-                                input = "";
-                                // pOutcome is not changed
-                            }
-                        } else {
-                            input = "";
-                            //pOutcome = "noAction";
-                            System.out.println("Invalid action. Please enter again.");
-                        }
+                        defaultHandler();
                 }
             }
-            return new String[]{input, pOutcome};
+            //return new String[]{this.encInput, this.outcome};
         }
 
-        public String[] enemyAction() {
-            String input = "";
-            String eOutcome = "";
-            System.out.println(enemy.getName() + " has made a move!");
-            eOutcome = enemy.attack(player);
-            // Resolve actions
-            if (!eOutcome.equals("pLose")) {
-                System.out.println("Your HP is " + player.getHp());
-            } else {
-                input = "back";
-                System.out.println("You have been defeated by " + enemy.getName() + ". Reviving yourself...");
-            }
-            return new String[]{input, eOutcome};
-        }
-
-        public String postturn(String input, String previousOutcome) {
-            //switch (input)
-            String turnOutcome = previousOutcome;
-            String[] inputParts = input.split("\\s");
-            switch (inputParts[0]) {
-                case "area":
-                    if (inputParts.length >= 2) {
-                        //int invPos = -1;
-                        try {
-                            Adventure.this.areaId = Integer.parseInt(inputParts[1]);
-                            turnOutcome = "switchArea";
-                            System.out.println("You are now in area " + areaId + ".");
-                        } catch (NumberFormatException numberFormatException) {
-                            System.out.println("REDUNDANT/Area index must be a number.");
-                            break;
+        public void defaultHandler() {
+            switch (this.encInput) {
+                case "where":
+                    reportScreen();
+                    System.out.println("'back' - Back to Town");
+                    this.encInput = "";
+                    //pOutcome = "noAction";
+                    break;
+                case "quit":
+                    System.out.println("Thanks for playing!");
+                    System.exit(0);
+                default:
+                    if (connections.get(this.encInput) != null) {
+                        System.out.println("Do you really want to leave this area? ('y' to confirm)");
+                        if (reader.nextLine().equals("y")) {
+                            // input is not changed
+                            this.outcome = "switchScreen";
+                        } else {
+                            this.encInput = "";
+                            // pOutcome is not changed
                         }
+                    } else {
+                        this.encInput = "";
+                        //pOutcome = "noAction";
+                        System.out.println("Invalid action. Please enter again.");
                     }
             }
-            return turnOutcome;
+        }
+
+        private void enemyAction() {
+            //String this.encInput = "";
+            //String this.outcome = "";
+            System.out.println(enemy.getName() + " has made a move!");
+            this.outcome = enemy.attack(player);
+            // Resolve actions
+            if (!this.outcome.equals("pLose")) {
+                System.out.println("Your HP is " + player.getHp());
+            } else {
+                this.encInput = "back";
+                System.out.println("You have been defeated by " + enemy.getName() + ". Reviving yourself...");
+            }
+            //return new String[]{this.encInput, this.outcome};
+        }
+
+        private void postturn() {
+
         }
     }
 }
